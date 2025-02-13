01118514cc3b7ca3c2422d6421185428e27e4d48
AzaanTechzilla <azaan.khalid@techzillaglobal.com>
2025-02-13 17:00:30 +0500

    small change

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -9,7 +9,7 @@ const Section3 = () => {
             <div className="flex lg:flex-row flex-col" style={{minHeight: "calc(100vh - 15vh)"}}>
             <div className="content basis-[100%] lg:basis-[50%] self-center px-4">
               <p className="text-[2rem] lg:text-[3rem] leading-none font-sofia font-light">Building a successful company can feel like a rollercoaster ride. Compassion alone won't get you there.</p>
-              <p className="text-[3rem] text-[5rem] font-black uppercase">We will.</p>
+              <p className="text-[3rem] lg:text-[5rem] font-black uppercase">We will.</p>
             </div>
             <div className="image basis-[100%] lg:basis-[50%] self-start px-2">
               <figure className="w-full h-full">
